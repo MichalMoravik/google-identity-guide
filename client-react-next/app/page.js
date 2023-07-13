0ab8b11772f4ae8450ae00c60bc746a8ecb04b2c
MichalMoravik <michal.moravik@icloud.com>
2023-07-13 20:50:45 +0200

    client-react-next - handling redirections based on claims

--- client-react-next/app/page.js
+++ client-react-next/app/page.js
@@ -1,15 +1,18 @@
 'use client';
 import { auth } from './firebaseConfig';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const provider = new GoogleAuthProvider();
+  const router = useRouter();
 
   const signInWithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, provider);
-      const { claims } = await auth.currentUser.getIdTokenResult();
-      console.log(claims);
+      await signInWithPopup(auth, provider);
+
+      const { claims } = await auth.currentUser.getIdTokenResult(true);
+      if (claims.role === 'admin') router.push('/admin');
     } catch (error) {
       console.log(error);
     }
