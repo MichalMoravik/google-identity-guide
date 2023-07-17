78aae2c760c704262d603f55255ea0fea0cbf2a9
MichalMoravik <michal.moravik@icloud.com>
2023-07-17 16:35:01 +0200

    client-react-next - fixed the functions sending emails

--- client-react-next/auth-email/app/page.js
+++ client-react-next/auth-email/app/page.js
@@ -4,7 +4,8 @@ import { auth } from './firebaseConfig';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  sendEmailVerification
+  sendEmailVerification,
+  signOut
 } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 
@@ -15,21 +16,18 @@ export default function Home() {
   const [signInPassword, setSignInPassword] = useState('');
   const [regEmail, setRegEmail] = useState('');
   const [regPassword, setRegPassword] = useState('');
-  const [error, setError] = useState('');
+  const [msg, setMsg] = useState('');
 
   const registerWithEmail = async (email, password) => {
     try {
       const cred = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(cred.user);
 
-      await sendEmailVerification(auth.currentUser, {
-        // change this based on your domain (e.g. through env vars)
-        // Important: add this domain to the email/password provider's "Authorized domains" list
-        url: 'http://localhost:3000'
-      });
-      console.log('Email Sent');
+      await sendEmailVerification(cred.user, { url: 'http://localhost:3000' });
+      setMsg('Please verify your email before signing in');
+
+      await signOut(auth);
     } catch (err) {
-      console.log(err);
+      console.log('Unexpected error: ', err);
     }
   }
 
@@ -38,33 +36,26 @@ export default function Home() {
       await signInWithEmailAndPassword(auth, email, password);
 
       const { claims } = await auth.currentUser.getIdTokenResult(true);
-      console.log("claims: ", claims);
+
+      if (!claims.email_verified) {
+        await sendEmailVerification(auth.currentUser, { url: 'http://localhost:3000' });
+        setMsg('Please verify your email before signing in. We have sent you another verification email');
+        await signOut(auth);
+        return;
+      }
+
+      console.log('Signed in as: ', claims.email);
 
       if (claims.role === 'admin') router.push('/admin');
     } catch (err) {
-      if (err.code !== 'auth/internal-error') {
-          console.log('Unexpected error: ', err);
-      } else {
-        const regStatus = err.message.match(/"status":"(.*?)"/);
-        const status = regStatus ? regStatus[1] : '';
-        const regMessage = err.message.match(/"message":"(.*?)"/);
-        const message = regMessage ? regMessage[1] : '';
-
-        if (status === 'INVALID_ARGUMENT') {
-          if (message === 'UNVERIFIED_EMAIL') {
-            setError('Please verify your email before signing in. We just sent you another email');
-
-            await sendEmailVerification(auth.currentUser, { url: 'http://localhost:3000' });
-          }
-        } else {
-          console.log('Unhandled blocking error: ', err);
-        }
-      }
+      console.log('Unexpected error: ', err);
     }
   }
 
   return (
     <div>
+      <div>{msg}</div>
+
       <h1>Registration</h1>
       <input
         type="text"
@@ -83,7 +74,6 @@ export default function Home() {
       </button>
 
       <h1>Sign in</h1>
-      {error && <p>{error}</p>}
       <input
         type="text"
         name="email"
