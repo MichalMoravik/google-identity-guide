7aef06b06b7f6c4dfe8b070bc8d72c695d0117f3
MichalMoravik <michal.moravik@icloud.com>
2023-07-17 15:08:59 +0200

    blocking-functions - removing the check for email_verified

--- blocking-functions/auth-email/handlers.js
+++ blocking-functions/auth-email/handlers.js
@@ -1,10 +1,4 @@
-import gcipCloudFunctions from 'gcip-cloud-functions';
-
 export const beforeSignInHandler = (user, context, adminEmails) => {
-  if (!user.emailVerified) {
-    throw new gcipCloudFunctions.https.HttpsError('invalid-argument', `UNVERIFIED_EMAIL`);
-  }
-
   const role = adminEmails.includes(user.email) ? 'admin' : 'user';
 
   console.log(`Signing in user: ${user.email} with role: ${role}`);
