cffd8135f3fcb8f3a8e717699080250e74012122
MichalMoravik <michal.moravik@icloud.com>
2023-07-18 16:52:16 +0200

    removed unused auth-email specific code

--- blocking-functions/auth-email/handlers.js
+++
@@ -1,9 +0,0 @@
-export const beforeSignInHandler = (user, context, adminEmails) => {
-  const role = adminEmails.includes(user.email) ? 'admin' : 'user';
-
-  console.log(`Signing in user: ${user.email} with role: ${role}`);
-  return {
-    customClaims: { role },
-  }
-};
-
--- blocking-functions/auth-email/index.js
+++
@@ -1,8 +0,0 @@
-import gcipCloudFunctions from 'gcip-cloud-functions';
-import { beforeSignInHandler } from './handlers.js';
-
-const adminEmails = process.env.ADMIN_EMAILS.split(',') || [];
-const authClient = new gcipCloudFunctions.Auth();
-
-export const beforeSignIn = authClient.functions()
-  .beforeSignInHandler((user, context) => beforeSignInHandler(user, context, adminEmails));
